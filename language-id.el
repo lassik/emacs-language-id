209d14287a3ebd8434eb26c1d564cb9e4da151e9
Lassi Kortela <lassi@lassi.io>
2019-01-25 13:48:23 +0200

    Update commentary

--- language-id.el
+++ language-id.el
@@ -13,13 +13,14 @@
 ;;
 ;; language-id is a small, focused library that helps other Emacs
 ;; packages identify the programming languages and markup languages
-;; used in Emacs buffers. The main point is that it contains an
+;; used in Emacs buffers.  The main point is that it contains an
 ;; evolving table of language definitions that doesn't need to be
 ;; replicated in other packages.
 ;;
-;; Currently the public API has just one function,
-;; `language-id-buffer'.  It looks at the major mode and other
-;; variables and returns the language's GitHub Linguist identifier.
+;; Right now there is only one public function, `language-id-buffer'.
+;; It looks at the major mode and other variables and returns the
+;; language's GitHub Linguist identifier.  We can add support for
+;; other kinds of identifiers if there is demand.
 ;;
 ;; This library does not do any statistical text matching to guess the
 ;; language.

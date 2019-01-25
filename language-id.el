2d09b17fa52daa1474f2824b55382c3bbb5ee265
Lassi Kortela <lassi@lassi.io>
2019-01-25 12:51:51 +0200

    Add boilerplate elisp

---
+++ language-id.el
@@ -0,0 +1,18 @@
+;;; language-id.el --- Library to work with programming language identifiers -*- lexical-binding: t -*-
+;;
+;; Author: Lassi Kortela <lassi@lassi.io>
+;; URL: https://github.com/lassik/emacs-language-id
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24"))
+;; Keywords: languages util
+;; License: MIT
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(provide 'language-id)
+
+;;; language-id.el ends here

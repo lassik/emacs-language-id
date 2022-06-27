e7f743e50c2da3fc18db8ae0ec8e09aa9154414b
Lassi Kortela <lassi@lassi.io>
2022-06-27 20:56:21 +0300

    Move languages.{sh,yml} to admin/

--- admin/languages.sh
+++ admin/languages.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-set -eux
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+set -x
 wget -O languages.yml https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml
